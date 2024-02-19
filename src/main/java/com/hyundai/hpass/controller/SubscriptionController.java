c05c044dc7b28df9823f552210d195c41644243f
최현서 <117807082+ha01032@users.noreply.github.com>
2024-02-19 13:55:35 +0900

    feat: 부트페이 토큰값 테스트

--- src/main/java/com/hyundai/hpass/controller/SubscriptionController.java
+++ src/main/java/com/hyundai/hpass/controller/SubscriptionController.java
@@ -1,17 +1,17 @@
 package com.hyundai.hpass.controller;
 
-import com.hyundai.hpass.dto.LoginResDto;
 import com.hyundai.hpass.service.SubscriptionService;
+import kr.co.bootpay.Bootpay;
+import kr.co.bootpay.model.request.UserToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import oracle.jdbc.proxy.annotation.Post;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/subscription")
@@ -19,10 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class SubscriptionController {
     private final SubscriptionService subscriptionService;
+    @Value("${bootpay.api-key}")
+    private String bootKey;
+    
+    
+    //이후 수정할 예정
+    @GetMapping
+    public ResponseEntity<String> getPaymentToken(
+            Authentication authentication
+    ) {
+        Bootpay bootpay = new Bootpay("65cc38c400be04001d1f294d","GmJRQaPf1FtOmjWQHK38jM4azJxzR3ev/mwtmhp9jlc=");
+        UserToken userToken = new UserToken();
+        userToken.userId = authentication.getName();
+
+        try {
+            bootpay.getAccessToken();
+            HashMap<String, Object> res = bootpay.getUserToken(userToken);
+            if(res.get("error_code") == null) { //success
+                System.out.println("getUserToken success: " + res);
+            } else {
+                System.out.println("getUserToken false: " + res);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<> ("", HttpStatus.ACCEPTED);
+    }
     @PostMapping
     public ResponseEntity<String> addSubscriber(
-            @RequestParam String payment,
-            Authentication authentication
+            Authentication authentication,
+            @RequestParam String payment
+
     ) {
         log.debug("결제 방법 : " + payment);
         log.debug("회원아이디 : " + authentication.getName());
